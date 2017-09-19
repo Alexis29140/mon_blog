9aaaf9f8d9ec5e2700c503f9784942b55417ee0d
Alexis29140 <alexis.lonney29140@gmail.com>
2017-09-19 16:07:37 +0200

    Version 3

--- index.php
+++ index.php
@@ -2,8 +2,15 @@
 
   require 'Modele.php'; 
   
-  $billets = getBillets();
- 
-  require 'vueAccueil.php';  
+  try 
+  {
+  	$billets = getBillets();
+  	require 'vueAccueil.php';
+  }
+  catch (Exception $e) 
+  {
+  	$msgErreur = $e->getMessage();
+  	require 'vueErreur.php';
+  }
   
   ?>

198bdcc9019a62202d92da3d46127168800d7e08
Alexis29140 <alexis.lonney29140@gmail.com>
2017-09-19 16:21:32 +0200

    Version 4

--- Modele.php
+++ Modele.php
@@ -7,8 +7,6 @@ $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
 . ' order by BIL_ID desc');
 return $billets;
 }
-
-
 // Effectue la connexion à la BDD
 // Instancie et renvoie l'objet PDO associé
 function getBdd() {
@@ -17,4 +15,27 @@ $bdd = new PDO('mysql:host=localhost;dbname=mon_blog;charset=utf8',
 return $bdd;
 }
 
- ?>
+
+// Renvoie les informations sur un billet
+function getBillet($idBillet) {
+  $bdd = getBdd();
+  $billet = $bdd->prepare('select BIL_ID as id, BIL_DATE as date,'
+    . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+    . ' where BIL_ID=?');
+  $billet->execute(array($idBillet));
+  if ($billet->rowCount() == 1)
+    return $billet->fetch();  // Accès à la première ligne de résultat
+  else
+   throw new Exception("Aucun billet ne correspond à l'identifiant '$idBillet'");
+}
+
+// Renvoie la liste des commentaires associés à un billet
+function getCommentaires($idBillet) {
+  $bdd = getBdd();
+  $commentaires = $bdd->prepare('select COM_ID as id, COM_DATE as date,'
+    . ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE'
+    . ' where BIL_ID=?');
+  $commentaires->execute(array($idBillet));
+  return $commentaires;
+}
+?>
